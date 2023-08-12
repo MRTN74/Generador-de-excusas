521f482697096345908c38f910f229c4d464fd49
Martin Gallo <136949107+MRTN74@users.noreply.github.com>
2023-08-12 04:09:56 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 // Using javascript, create a function that generates and returns a random excuse with the following structure:
+import "./style.css";
+
 let who = ["The dog", "My grandma", "His turtle", "My bird"];
 let what = ["ate", "peed", "crushed", "broke"];
+let action = ["my homework", "the keys", "the car"];
 let when = [
   "before the class",
   "right on time",
@@ -8,24 +11,24 @@ let when = [
   "during my lunch",
   "while I was praying"
 ];
-let action = ["my homework", "the keys", "the car"];
 
-function excuseGenerator(quien, que, cuando) {
+function excuseGenerator(quien, que, cuando, accion) {
   let positionQuien = Math.floor(Math.random() * who.length);
   let positionQue = Math.floor(Math.random() * what.length);
   let positionCuando = Math.floor(Math.random() * when.length);
+  let positionAccion = Math.floor(Math.random() * action.length);
   return (
     quien[positionQuien] +
     " " +
     que[positionQue] +
     " " +
-    "my homework" +
+    accion[positionAccion] +
     " " +
     cuando[positionCuando]
   );
 }
 function onLoad() {
   var excusa = document.getElementById("excuse");
-  excusa.innerHTML = excuseGenerator(who, what, when);
+  excusa.innerHTML = excuseGenerator(who, what, when, action);
 }
 window.onload = onLoad;
